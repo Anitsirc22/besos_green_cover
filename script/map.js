676e9d066f86e1c611d88464093e47b92f213e4f
Cristina Ferrer <cris.ferrer22@gmail.com>
2019-02-10 15:33:39 +0100

    update map scripts generating scale errors

--- script/map.js
+++ script/map.js
@@ -1,27 +1,36 @@
 let map=d3.select("#map");
-d3.json("json/ndvi_reclass_2014.json").then(showData);
-function showData (json_map){
-	let mapHeight=400
-	let mapWidth=400
+map.attr("width",window.innerWidth)
+map.attr("height",window.innerHeight)
+
+function fetchMapData () {
+	return d3.json("json/ndvi_reclass_2014.json");	
+}
+
+function showData (json_map, bounds){
+	let mapHeight=window.innerHeight
+	let mapWidth=window.innerWidth
 	//console.log(json_map)
 
 	
-	var projection=d3.geoMercator()
-		.center(d3.geoCentroid(json_map))
-		.scale(50)
-		.translate([mapWidth/2,mapHeight/2])
+	// var projection=d3.geoMercator()
+	// 	.center(d3.geoCentroid(json_map))
+	// 	.scale(50)
+	// 	.translate([mapWidth/2,mapHeight/2])
+		
+	// var center=d3.geoCentroid(json_map)
+	var center = [Math.abs(bounds[1][0] - bounds[0][0])/2, Math.abs(bounds[1][1] - bounds[0][1])/2]
+	var scale  = Math.abs(bounds[1][0] - bounds[0][0])/mapWidth
+/*	var bounds  = path.bounds(json_map);*/
+    // var hscale  = scale*mapWidth  / Math.abs(bounds[1][0] - bounds[0][0]);
+    // var vscale  = scale*mapHeight / Math.abs(bounds[1][1] - bounds[0][1]);
+    // var scale   = (hscale < vscale) ? hscale : vscale;
+    var offset  = [mapWidth - (bounds[0][0] + bounds[1][0])/2,mapHeight - (bounds[0][1] + bounds[1][1])/2];
+    projection = d3.geoMercator()
+    	.center(center)
+    	.scale(scale)
+    	// .translate(offset);
 	var path=d3.geoPath()
 		.projection(projection);
-		
-	var center=d3.geoCentroid(json_map)
-	var scale  = 150;
-	var bounds  = path.bounds(json_map);
-    var hscale  = scale*mapWidth  / (bounds[1][0] - bounds[0][0]);
-    var vscale  = scale*mapHeight / (bounds[1][1] - bounds[0][1]);
-    var scale   = (hscale < vscale) ? hscale : vscale;
-    var offset  = [mapWidth - (bounds[0][0] + bounds[1][0])/2,mapHeight - (bounds[0][1] + bounds[1][1])/2];
-    projection = d3.geoMercator().center(center).scale(scale).translate(offset);
-		path = path.projection(projection);
 
 	let max_range=d3.max(json_map.features,d=>d.properties.gridcode);
 	let cscale=d3.scaleLinear()
@@ -31,7 +40,7 @@ function showData (json_map){
 	map.selectAll("path")
 		.data(json_map.features)
 		.enter().append("path")
-		.attr("d",d=>path(d))
+		.attr("d",path)
 		.attr("stroke","none")
 		.attr("fill",d=>d.properties.gridcode?cscale(d.properties.gridcode):"none")
 		.attr("stroke-width","0.2")
