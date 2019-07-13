a5b2061b3476f3a423683191377d313cf37f6e38
Cristina Ferrer <cris.ferrer22@gmail.com>
2019-07-13 14:45:32 +0200

    solved merged conflict

--- csv/text.js
+++ csv/text.js
@@ -1,7 +1,7 @@
 var text_right= "<p>The Besòs River is a spanish river located in the province of Barcelona. It was considered one of the most polluted rivers in Europe in the last decades of the 20th entury as a result of the industries that were placed around it and the population increase that occurred at that time. From the construction of sewage plants along the river and the environmental recovery project of the final section of the river, many species of flora and fauna have been recovered.</p><p>This is a research on the evolution of the green cover in the river mouth. Images with infrared band information provided by the ICGC have been analyzed to calculate the NDVI values over the period 2009-2016. It is important to consider that photometric flights have been carried out in different months of the year, for this reason a non-continuous growth of the vegetation can be appreciated.</p>"
 
 var text_left=`
-  <p>Raster images have been processed to classify all patterns of vegetationaccording to the ranges defined by the ICGC (Cartographic and Geological Institute of Catalonia):<p>
+  <p>Raster images have been processed to classify all patterns of vegetation according to the ranges defined by the ICGC (Cartographic and Geological Institute of Catalonia):<p>
   <p>0 < NDVI <0.2: Bare soil or dead vegetation</p>
   <p>0.2 < NDVI <0.4: Scattered or little vigorous vegetation</p>
   <p>0.4 < NDVI <0.6: Abundant and vigorous vegetation</p>
